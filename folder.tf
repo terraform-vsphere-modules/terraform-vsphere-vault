f44b26edb8b8e09d2795fa342b0dd6671f3723a3
pandom <pandom@users.noreply.github.com>
2022-11-04 16:11:25 +1100

    Adjustment

--- folder.tf
+++ folder.tf
@@ -1,11 +1,15 @@
+data "vsphere_datacenter" "dc" {
+ name = local.vsphere_data.datacenter
+}
+
 resource "vsphere_folder" "private" {
   path          = "private-cloud"
   type          = "vm"
-  datacenter_id = local.vsphere_data.datacenter
+  datacenter_id = data.vsphere_datacenter.dc
 }
 
 resource "vsphere_folder" "management" {
   path          = "${vsphere_folder.private.path}/management-machines"
   type          = "vm"
-  datacenter_id = local.vsphere_data.datacenter
+  datacenter_id = data.vsphere_datacenter.dc
 }
