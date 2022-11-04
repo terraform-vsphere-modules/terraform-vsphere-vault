0bbd0dc4afe25118babbe62cf383c7bfc13392b0
pandom <pandom@users.noreply.github.com>
2022-11-04 16:16:25 +1100

    Strings

--- folder.tf
+++ folder.tf
@@ -5,11 +5,11 @@ data "vsphere_datacenter" "dc" {
 resource "vsphere_folder" "private" {
   path          = "private-cloud"
   type          = "vm"
-  datacenter_id = data.vsphere_datacenter.dc
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 resource "vsphere_folder" "management" {
   path          = "${vsphere_folder.private.path}/management-machines"
   type          = "vm"
-  datacenter_id = data.vsphere_datacenter.dc
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
